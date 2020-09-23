d5702a81336d866c9788b6325d418340001b3831
agrawalsrijan <47100634+agrawalsrijan@users.noreply.github.com>
2020-09-23 15:41:54 +0530

    Initializing Channels Component

--- src/components/SidePanel/SidePanel.jsx
+++ src/components/SidePanel/SidePanel.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Menu } from "semantic-ui-react"
 
 import UserPanel from "./UserPanel"
-
+import Channels from "./Channels"
 class SidePanel extends Component {
 
     render() {
@@ -16,6 +16,8 @@ class SidePanel extends Component {
                 style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
             >
                 <UserPanel currentUser={currentUser} />
+
+                <Channels />
             </Menu>
         )
     }

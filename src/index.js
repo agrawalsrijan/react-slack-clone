ab9224d2fffef01179228c4ea6d651d74e51270b
agrawalsrijan <47100634+agrawalsrijan@users.noreply.github.com>
2020-09-23 13:38:53 +0530

    Integrating State management with Redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React,  { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-// import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Switch, Route, withRouter } from "react-router-dom"
+
+// Imports for state management - redux
+import { createStore, applyMiddleware } from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import logger from "redux-logger";
+import rootReducer from "./reducers/index";
+import { setUser } from "./actions/index"
+
+// Firebase related imports
+import firebase from "./firebase"
+
 // Import components 
+import Spinner from "./Spinner"
 import App from './components/App';
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
@@ -10,21 +22,56 @@ import Register from "./components/Auth/Register";
 // import semantic ui styles
 import "semantic-ui-css/semantic.min.css"
 
-const Root = () => {
-  return(
-    <Router>
-      <Switch>
-        <Route exact path="/" component={App}/>
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-      </Switch>
-    </Router>
-  )
+const middlewares = [];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
 }
 
+const store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(...middlewares)
+))
+
+class Root extends Component{
+
+  componentDidMount(){
+    firebase.auth().onAuthStateChanged(user => {
+      if(user){
+        this.props.setUser(user)
+        this.props.history.push("/");
+      }
+    })
+  }
+
+  render() {
+    return this.props.isLoading ? <Spinner/> : (
+      
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      
+    )
+  }
+
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isLoading: state.user.isLoading
+  }
+}
+const RootWithAuth = withRouter(connect(mapStateToProps, { setUser })(Root));
+
 ReactDOM.render(
-  
-  <Root/>,
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>
+ 
+  ,
   document.getElementById('root')
 );
 

bcd8d04c78a71a2d75dcef0ec40fd3cedd5c2530
Srijan <47100634+agrawalsrijan@users.noreply.github.com>
2020-10-07 20:42:08 +0530

    update firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,7 @@ import "firebase/database";
 import "firebase/storage";
 
 
-var firebaseConfig = {
-    apiKey: "AIzaSyCLjzbqEzGPT1VFtL3Ce1EMRmEQAjJirts",
-    authDomain: "react-slack-clone-9f417.firebaseapp.com",
-    databaseURL: "https://react-slack-clone-9f417.firebaseio.com",
-    projectId: "react-slack-clone-9f417",
-    storageBucket: "react-slack-clone-9f417.appspot.com",
-    messagingSenderId: "727358299203",
-    appId: "1:727358299203:web:a7c2bd52bb3fbe6e1848f6"
-};
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
